d477d47f5361f448d9af59bd28a230a6c8534643
ASUS <mhn.work@yahoo.com>
2019-07-12 23:56:50 +0430

    Checking a github issue 2...

--- app/src/main/java/ir/mhn/textapicache/MainActivity.java
+++ app/src/main/java/ir/mhn/textapicache/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity implements TextAPICacheListe
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private TextView txtTest;
-    private TextAPICache apiCache = new TextAPICache(this, this, TextAPICacheMode.NORMAL_MODE);
+//    private TextAPICache apiCache = new TextAPICache(this, this, TextAPICacheMode.NORMAL_MODE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity implements TextAPICacheListe
         setContentView(R.layout.activity_main);
 
         txtTest = findViewById(R.id.txtTest);
-        apiCache = new TextAPICache(this, this, TextAPICacheMode.MOST_OFFLINE_MODE);
+//        apiCache = new TextAPICache(this, this, TextAPICacheMode.MOST_OFFLINE_MODE);
         TextAPICache textAPICache = new TextAPICache(this, new TextAPICacheListener() {
             @Override
             public void onCacheLoaded(String url, String cacheName, String cacheData) {
@@ -41,18 +41,18 @@ public class MainActivity extends AppCompatActivity implements TextAPICacheListe
             }
         }, TextAPICacheMode.NORMAL_MODE);
 
-        textAPICache
-                .get("https://en-maktoob.yahoo.com/?p=us", "YahooCache", false)
-                .get("https://github.com", "GitHubCache")
-                .get("https://www.microsoft.com/en-us/", true)
-                .get("https://www.apple.com/")
-                .get("https://jitpack.io/", "JitPackCache", false, 10000);
+//        textAPICache
+//                .get("https://en-maktoob.yahoo.com/?p=us", "YahooCache", false)
+//                .get("https://github.com", "GitHubCache")
+//                .get("https://www.microsoft.com/en-us/", true)
+//                .get("https://www.apple.com/")
+//                .get("https://jitpack.io/", "JitPackCache", false, 10000);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                apiCache.get("https://en-maktoob.yahoo.com/?p=us");
+//                apiCache.get("https://en-maktoob.yahoo.com/?p=us");
             }
         });
     }
